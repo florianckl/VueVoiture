057f2130c5640cf67914d155eced054634cb25b0
florian <florian200928@hotmail.fr>
2020-10-07 10:14:11 +0200

    init +images+put+Panier

--- src/components/CarsList.vue
+++ src/components/CarsList.vue
@@ -22,14 +22,12 @@
 <script>
 
 import http from "../http-common";
-//import * as axios from "axios";
 
 export default {
   name: "cars-list",
   data() {
     return {
       cars: [],
-      linke:"",
       files:null,
       src:"data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
     };
@@ -46,50 +44,13 @@ export default {
           .catch(e => {
             console.log(e);
           });
-      //const url ="http:localhost:8080/download/"+ "Capture2.png";
-
-      /*
-      http
-          .get(url).then(res=>{
-          this.files = res.data;
-        })
-      },*/
-/*
-        let config = {
-          // example url
-          url: "http://localhost:8080/download/"+"capture2.png",
-          method: 'GET',
-          responseType: 'arraybuffer'
-        }
-        // eslint-disable-next-line no-undef
-        axios(config)
-            .then((response) => {
-              var bytes = new Uint8Array(response.data);
-              var binary = bytes.reduce((data, b) => data += String.fromCharCode(b), '');
-              this.src = "data:image/png;base64," + btoa(binary);
-            });*/
     },
-
-      /*
-      http
-          .get(url, { responseType: 'blob' })
-          .then(response => {
-            const blob = new Blob([response.data], { type: 'application/*' })
-            const link = document.createElement('a')
-            link.href = URL.createObjectURL(blob)
-            //link.download = "Capture.png"
-            this.linke=link.href;
-            //link.click()
-            URL.revokeObjectURL(link.href)
-          }).catch(console.error)
-    },*/
     refreshList() {
       this.retrieveCars();
     }
     /* eslint-enable no-console */
   },
   mounted() {
-
     this.retrieveCars();
   }
 };
